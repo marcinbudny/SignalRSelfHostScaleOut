fc4ae3dbb5d50724f23ce7ea096b08e2c1794e62
Marcin Budny <marcin.budny@gmail.com>
2016-04-23 20:52:03 +0200

    sample code

---
+++ SignalRHost/SignalRScaleoutConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Fabric;
+
+namespace SignalRHost
+{
+    public static class SignalRScaleoutConfiguration
+    {
+        public static bool UseScaleout { get; }
+        public static string RedisConnectionString { get; }
+        public static string RedisAppName { get; } = "SignalRSelfHostScaleOut";
+        public static string EncryptionPassword { get; } = "no password";
+
+        static SignalRScaleoutConfiguration()
+        {
+            var codeContext = FabricRuntime.GetActivationContext();
+            if (codeContext == null) // sanity check
+                throw new ApplicationException("CodePackageActivationContext is null");
+
+            ConfigurationPackage configurationPackage = codeContext.GetConfigurationPackageObject("Config");
+
+            if (configurationPackage.Settings?.Sections == null || !configurationPackage.Settings.Sections.Contains("SignalRScaleout"))
+                return;
+
+            var param = configurationPackage.Settings.Sections["SignalRScaleout"].Parameters;
+
+            if (param.Contains("UseScaleout"))
+                UseScaleout = bool.Parse(param["UseScaleout"].Value);
+            if (param.Contains("RedisConnectionString"))
+                RedisConnectionString = param["RedisConnectionString"].Value;
+            if (param.Contains("RedisAppName"))
+                RedisAppName = param["RedisAppName"].Value;
+            if (param.Contains("EncryptionPassword"))
+                EncryptionPassword = param["EncryptionPassword"].Value;
+        }
+    }
+}
---
+++ SignalRHost/Startup.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Fabric;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using Owin.Security.AesDataProtectorProvider;
+
+namespace SignalRHost
+{
+    public static class Startup
+    {
+        public static void ConfigureApp(IAppBuilder app)
+        {
+            ConfigureCors(app);
+            ConfigureSignalR(app);
+            ConfigureStaticFiles(app);
+
+            RunIncrementingTask();
+        }
+
+        private static void ConfigureCors(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+        }
+
+        private static void ConfigureSignalR(IAppBuilder app)
+        {
+            app.UseAesDataProtectorProvider(SignalRScaleoutConfiguration.EncryptionPassword);
+
+            if (SignalRScaleoutConfiguration.UseScaleout)
+            {
+                var redisConfig = new RedisScaleoutConfiguration(
+                    SignalRScaleoutConfiguration.RedisConnectionString,
+                    SignalRScaleoutConfiguration.RedisAppName);
+                GlobalHost.DependencyResolver.UseRedis(redisConfig);
+            }
+
+            var configuration = new HubConfiguration { EnableDetailedErrors = true };
+            app.MapSignalR(configuration);
+        }
+
+        private static void ConfigureStaticFiles(IAppBuilder app)
+        {
+            var fileSystem = new PhysicalFileSystem(@".\wwwroot");
+            var options = new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                RequestPath = PathString.Empty,
+                FileSystem = fileSystem,
+            };
+
+            options.DefaultFilesOptions.DefaultFileNames = new[] { "index.html" };
+            options.StaticFileOptions.FileSystem = fileSystem;
+
+            app.UseFileServer(options);
+        }
+
+        private static void RunIncrementingTask()
+        {
+            Task.Factory.StartNew(async () =>
+            {
+                var random = new Random();
+
+                while (true)
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(random.Next(500, 2000)));
+
+                    var message = $"Hello from node {FabricRuntime.GetNodeContext().NodeId}";
+
+                    GlobalHost.ConnectionManager.GetHubContext<HelloHub>()
+                        .Clients.All
+                        .notifyNewMessage(message);
+                }
+            }, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+    }
+}
